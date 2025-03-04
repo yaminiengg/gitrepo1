f118615a14af37f9c86c9034ac760f3ff3233b00
yamini <yaminip222@gmail.com>
2025-03-04 06:16:31 +0000

    commits

--- alpha.py
+++ alpha.py
@@ -1,3 +1,4 @@
+#CHARACTER-ASCII CODE
 val= input()
 if(ord(val)>=65 and ord( val)<=90):
     print(" upper case alphabets")
@@ -5,5 +6,6 @@ elif(ord(val)>=97 and ord(val)<=122):
     print("lower alphabets")
     
 elif(ord(val)>=48 and ord(val)<=57):
+           print("Numbers")
 else:
     print("special character")

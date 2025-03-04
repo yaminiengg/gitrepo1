2ee75ac91bc7571c4d20fe7ee76a584dabbe0007
V KARTHIKEYA <86949252+karthik1525@users.noreply.github.com>
2025-03-04 10:51:38 +0530

    Add files via upload

---
+++ alpha.py
@@ -0,0 +1,9 @@
+val= input()
+if(ord(val)>=65 and ord( val)<=90):
+    print(" upper case alphabets")
+elif(ord(val)>=97 and ord(val)<=122):
+    print("lower alphabets")
+    
+elif(ord(val)>=48 and ord(val)<=57):
+else:
+    print("special character")
